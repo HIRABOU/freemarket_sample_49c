9feb70a0d1f5c0be2650672916d331a596be953b
HIRABOU <owner@ownernoMacBook-puro.local>
2019-04-28 16:09:24 +0900

    Create product_detail_page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'items/edit_user_info' => 'items#edit_user_info'
   get 'items/profile_edit' => 'items#profile_edit'
   root 'items#index'
-
+  get 'items/product_detail_page' => 'items#product_detail_page'
   get 'signup' => 'users#signup'
 
 
