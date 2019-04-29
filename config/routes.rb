b48648c99975791b21d7104a0b28072cadabb35d
HIRABOU <owner@ownernoMacBook-puro.local>
2019-04-29 11:59:37 +0900

    Cure conflict

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,11 @@ Rails.application.routes.draw do
   get 'items/edit_user_info' => 'items#edit_user_info'
   get 'items/profile_edit' => 'items#profile_edit'
   root 'items#index'
-<<<<<<< HEAD
   get 'items/product_detail_page' => 'items#product_detail_page'
-=======
   resources :items do
     collection do
       get 'sell'
     end
   end
->>>>>>> master
   get 'signup' => 'users#signup'
 end
