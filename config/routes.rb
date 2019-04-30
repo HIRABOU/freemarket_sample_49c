b28af4be84ad2e5451a5611295c48fa6c364a352
HIRABOU <owner@ownernoMacBook-puro.local>
2019-04-30 13:57:23 +0900

    Change name
    product_detail_page => show

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'items/edit_user_info' => 'items#edit_user_info'
   get 'items/profile_edit' => 'items#profile_edit'
   root 'items#index'
-  get 'items/product_detail_page' => 'items#product_detail_page'
+  get 'items/show' => 'items#show'
   resources :items do
     collection do
       get 'sell'
