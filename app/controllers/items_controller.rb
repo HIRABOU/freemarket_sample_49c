b78c1fcb5e065bab182db30d9cebb605070f73a6
yuu0305 <segyayuu8@gmail.com>
2019-05-07 16:19:57 +0900

    Almost done
    almost

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
@@ -7,7 +8,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new
+    # @item = Item.new
     @items = Item.create(item_params)
     render :index
   end
@@ -27,10 +28,17 @@ class ItemsController < ApplicationController
   def profile_edit
   end
 
+  def show
+    @item = Item.find(params[:id])
+    @user = @item.user
+    @other_items = Item.where.not(id: params[:id])
+  end
+
   private
 
   def item_params
     params.permit(:name, :text, :state, :delivery_fee, :shipping, :price, :category_id).merge(user_id: current_user.id)
   end
+
 
 end
