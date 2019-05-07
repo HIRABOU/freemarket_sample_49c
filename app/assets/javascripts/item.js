51cfd13893c4f75b8454d1251c6504fa9e9d81ff
HIRABOU <owner@ownernoMacBook-puro.local>
2019-05-07 17:46:35 +0900

    Create item.js
    手数料と利益計算の追加

---
+++ app/assets/javascripts/item.js
@@ -0,0 +1,36 @@
+$(function(){
+
+  // 数字をカンマ区切り
+  function separate(num){
+    // 文字列にする
+    num = String(num);
+    var len = num.length;
+
+    // 再帰的に呼び出す
+    if(len > 3){
+        // 前半を引数に再帰呼び出し + 後半3桁
+        return separate(num.substring(0,len-3))+','+num.substring(len-3);
+    } else {
+        return num;
+    }
+  }
+  
+  // 手数料と利益計算
+  $("#item_price").on('keyup',$("#item_price"), function() {
+    var price = $(this).val();
+    var mercari_margin = price * 0.1 ;
+    var mercari_margin = Math.floor(mercari_margin);
+    var saler_margin = price - mercari_margin;
+    var mercari_margin = separate(mercari_margin);
+    var saler_margin = separate(saler_margin);
+  
+  if(price >= 300 && price <= 9999999){
+    $('#price_fee').text("¥"+mercari_margin);
+    $('#sales_profit').text("¥"+saler_margin);
+    }
+  else{
+    $('#price_fee').text("-");
+    $('#sales_profit').text("-");
+    }
+  });
+});
