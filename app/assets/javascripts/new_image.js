e718373fb214dfa11bd26ae83eb27e43bb111d0d
HIRABOU <owner@ownernoMacBook-puro.local>
2019-05-09 12:43:19 +0900

    クラス名を変更

--- app/assets/javascripts/new_image.js
+++ app/assets/javascripts/new_image.js
@@ -12,17 +12,16 @@ $(function(){
     var reader = new FileReader();
     var img = new Image();
     var html = `<div class="preview_box_container" >
-        <div class="preview_box_container__edit">
+        <div class="preview__edit">
           編集
         </div>
-        <div class="preview_box_container__delete">
+        <div class="preview__delete">
           削除
         </div>
       </div>`
     var image_box = $('<div class="sell_image_box">');
     image_box.append(img)
     image_box.append(html)
-  console.log(image_box)
     var def =$.Deferred();
     reader.onload = function(e){
       $('.preview').append(image_box);
@@ -31,12 +30,18 @@ $(function(){
       img.height = 116
       def.resolve(image_box);
     };
+    var list = $('.sell_image_box');
+    if(list.length >= 9){
+        $('#images_field').css('display','none');
+     }
     reader.readAsDataURL(imageFile);
     return def.promise();
+
+    
   }
 
   //previewリストを削除。
-  $(document).on('click','.preview_box_container__delete',function(){
+  $(document).on('click','.preview__delete',function(){
     //押された削除ボタンの親要素(リスト本体)を取得。
     var list = $(this).parent().parent();
     //リストの番号を取得。
@@ -46,9 +51,5 @@ $(function(){
     //両方を削除。これでpreviewで出している画像とinputで送る画像を消すことで、差異を無くしている。
     label.remove();
     list.remove();
-    //ここでlistの数が9個以下なら最新のinput(中身が入っていない)を表示している。
-    if(list.length < 9){
-      $('.sell-upload-box-drop-box:last').css('display','block');
-    }
   });
 });
