66997006b6a5674b407c76e5f861d4b27cde21ec
HIRABOU <owner@ownernoMacBook-puro.local>
2019-05-09 21:02:48 +0900

    画像プレビューの整理

--- app/assets/javascripts/new_image.js
+++ app/assets/javascripts/new_image.js
@@ -1,10 +1,9 @@
 fileCount = 0;
 $(function(){
-
   function previewImage(i, imageFile){
     var reader = new FileReader();
     fileCount += 1;
-    var img = new Image();
+    var img = new Image(114, 116);
     var html = `<div class="preview_box_container id=${i}"} >
         <div class="preview__edit">
           編集
@@ -18,11 +17,14 @@ $(function(){
     image_box.append(html)
     var def =$.Deferred();
     reader.onload = function(){
-      $('.preview').append(image_box);
       img.src = reader.result;
-      img.width = 114
-      img.height = 116
+      var row = parseInt(fileCount/5 + 1);
+      var column = parseInt(fileCount%5 + 1);
+      img.style = "grid-column:" + (column-1) + "/" + (column) + ";"
+      img.style = "grid-row:" + (row) + "/" + (row+1) + ";"
       img.setAttribute("id", "image_" + i)
+      $('.image-grid-box').append(image_box);
+      $('#images_field').css({"grid-column":`${column}/6`, "grid-row":`${row}/2`});
       def.resolve(image_box);
     };
     reader.readAsDataURL(imageFile);
@@ -32,48 +34,25 @@ $(function(){
 
 
   $('#images-file1').on('change',function(){
+    
     files = this.files;
     var d = (new $.Deferred()).resolve();
     $.each(files,function(i,file){
-      console.log(file)
       if (fileCount >= 9){
         $('#images_field').css('display','none');
       };
-      if (fileCount >= 10) {
+      if (fileCount + files.length > 10) {
         document.getElementById("itemimage_uproad_over").style.display="block";
         return false;
       };
       d = d.then(function(){return previewImage(i, file)});
-      d = d.then(function(){return deleteImage(i)});
     });
   })
 
-
-  function deleteImage(i){
+    //プレビュー画像の削除
     $(document).on('click','.preview__delete',function(){
       var list = $(this).parent().parent();
       list.remove();
-      delete FileList[i];
     });
-  }
-
-
-
-
-
-
-  // //previewリストを削除。
-  // $(document).on('click','.preview__delete',function(){
-  //   //押された削除ボタンの親要素(リスト本体)を取得。
-  //   var list = $(this).parent().parent();
-  //   //リストの番号を取得。
-  //   var parentValue = list.attr('id');
-  //   console.log(parentValue)
-  //   // var file = files[parentValue]
-  //   //両方を削除。これでpreviewで出している画像とinputで送る画像を消すことで、差異を無くしている。
-  //   // label.remove();
-  //   list.remove();
-  //   // file.remove();
-  // });
 });
 
