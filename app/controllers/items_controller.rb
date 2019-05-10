4806a46e63071886f625359450f48d11e1e392f3
yuu0305 <segyayuu8@gmail.com>
2019-05-10 15:40:49 +0900

    commit
    修正しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show]
   def index
-    @items = Item.all.first(4)
+    @items = Item.first(4)
   end
 
   def new
@@ -28,7 +29,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
     @user = @item.user
     @other_items = Item.where.not(id: params[:id])
     @image = @item.images.first
@@ -40,5 +40,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :text, :state, :delivery_fee, :shipping, :price, :category_id, images: []).merge(user_id: current_user.id)
   end
 
-
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
