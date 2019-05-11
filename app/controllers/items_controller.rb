764d6146361beee2a6ccd01ecf3b924656602467
HIRABOU <owner@ownernoMacBook-puro.local>
2019-05-11 14:06:33 +0900

    Add order
    降順に変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show]
   def index
-    @items = Item.first(4)
+    @items = Item.order(id: "DESC").first(4)
   end
 
   def new
