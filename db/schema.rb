c6229117073d8a8db5281644f73395eb0adff73d
HIRABOU <owner@ownernoMacBook-puro.local>
2019-05-13 19:35:51 +0900

    Tradeカラムの追加
    itemsにboolean型tradeカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_11_054117) do
+ActiveRecord::Schema.define(version: 2019_05_13_103215) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2019_05_11_054117) do
     t.string "shipping", null: false
     t.string "delivery_fee", null: false
     t.integer "prefecture", null: false
+    t.boolean "trade", default: false, null: false
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
