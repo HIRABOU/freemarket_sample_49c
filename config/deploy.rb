c676f09aec36c8b6cd2d6677a364b2184d30f14b
morry48 <yyyyuuuuuuuu0408@gmail.com>
2019-05-15 11:48:50 +0900

    firstcommit

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,8 @@ set :default_env, {
   BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
 }
 
+set :linked_files, %w{config/master.key}
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
+                 registrations: 'users/registrations' }
+
   root 'items#index'
   resources :users do
     collection do
       get 'signup'
-      get 'mypage_card_create'
       get 'mypage_card'
-      get 'mypage'
       get 'log_out'
+      post 'pay'
     end
   end
   resources :items do
@@ -15,9 +17,34 @@ Rails.application.routes.draw do
       get 'edit_user_info'
       get 'profile_edit'
       get 'purchase'
-
+      get 'search'
+      get 'item_confirmation'
     end
   end
 
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
+
+   resources :purchase, only: [:index] do
+      collection do
+      get 'index', to: 'purchase#index'
+      post 'pay', to: 'purchase#pay'
+      get 'done', to: 'purchase#done'
+      end
+    end
+
+  resources :exchanges, only: [:show] do
+    collection do
+      get 'listing_confirmation'
+      get 'trading'
+      get 'sold'
+    end
+  end
 end
+
 
